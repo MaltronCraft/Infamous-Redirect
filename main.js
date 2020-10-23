56b93c08a2e429d57a347b023fefbe3b03650e94
MaltronCraft Network <41383245+MaltronCraft@users.noreply.github.com>
2020-10-22 22:37:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express()
 
-app.use('*',  createProxyMiddleware({ target: 'http://web.infamouscluster.tk/', port: 25627, changeOrigin: true }))
+app.use('*',  createProxyMiddleware({ target: 'http://web.infamous.tk/', port: 25627, changeOrigin: true }))
 
 app.listen(8080)
