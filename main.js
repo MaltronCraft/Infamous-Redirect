e9a100d1dc6ebb725ce2959e44c5ce166603c13c
MaltronCraft Network <41383245+MaltronCraft@users.noreply.github.com>
2020-10-22 22:16:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ var express = require('express')
 var { createProxy } = require('http-proxy-middleware');
 var fs = require('fs');
 
-var proxyServer = proxy
-
 var app = express()
 
 app.use('*',  createProxy({ target: 'http://web.infamouscluster.tk/', port: 25627, changeOrigin: true }))
